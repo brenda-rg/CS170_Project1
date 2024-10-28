393ca8cf1a97d147eb0afee2630f8ab8b10ccbbb
Abigail Orozco <aoroz064@ucr.edu>
2024-10-28 00:22:52 -0700

    main

--- main.cpp
+++ main.cpp
@@ -11,8 +11,12 @@ using namespace std;
 
 int main() {
     //default 8-puzzle positions
-    vector<int> startConfig[0,1,2,3,4,5,6,7,8];
+    vector<int> startConfig{0,1,2,3,4,5,6,7,8};
 
     cout << "testing..." << endl;
+    cout << "Welcome to 862228377 8 puzzle solver." << endl;
+    cout << "Type '1' to use a default puzzle, or '2' to enter your own puzzle." << endl;
+    cin int;
+    if (int = 1 )
     return 0;
 }
