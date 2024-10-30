ec1c11b4f7107a2d93eea4e51cb1122125919a32
brenda-rg <broja016@ucr.edu>
2024-10-29 20:57:01 -0700

    fix uniform cost function: code needs to be tidied up

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <cstring>
 #include <math.h>
 #include "problem.cpp"
+#include "search.cpp"
 using namespace std;
 
 int main() {
@@ -18,10 +19,12 @@ int main() {
     vector<int> row1;
     vector<int> row2;
     vector<int> row3;
+    Problem p;
     int var;
 
     cout << endl <<"Welcome to XXX (change this to your student ID) 8 puzzle solver." << endl
     << "Type “1” to use a default puzzle, or “2” to enter your own puzzle." << endl;
+
     cin >> var;
     if (var == 2) {
 
@@ -30,28 +33,63 @@ int main() {
         for(int i = 0; i < 3; i++) {
             cin >> var;
             row1.push_back(var);
+            startConfig.at(i) = var;
         }
         cout << endl << "Enter the second row, use space or tabs between numbers" << endl;
         for(int i = 0; i < 3; i++) {
             cin >> var;
-            row1.push_back(var);
+            row2.push_back(var);
+            startConfig.at(i+3) = var;
         }
         cout << endl << "Enter the third row, use space or tabs between numbers" << endl;
         for(int i = 0; i < 3; i++) {
             cin >> var;
-            row1.push_back(var);
+            row3.push_back(var);
+            startConfig.at(i+6) = var;
         }
+        p = Problem(startConfig);
+    }
+    else {
+        p = Problem();
     }
     // check if valid (ie: no repeats and values only from 0-9)
     //throw exception?
-
-
+    searchQ search;
 
     cout << endl << "Enter your choice of algorithm (Please type the number of your choice)" << endl
-    << "1. Uniform Cost Search" << endl
-    << "2. A* with the Misplaced Tile heuristic." << endl
-    << "3. A* with the Euclidean distance heuristic" << endl;
+        << "1. Uniform Cost Search" << endl
+        << "2. A* with the Misplaced Tile heuristic." << endl
+        << "3. A* with the Euclidean distance heuristic" << endl;
+    cin >> var;
+    while(true) {
+
+        if(var == 1) {
+            search.UCS(p);
+            cout << "test1" << endl;
+            break;
+        }
+
+        else if(var == 2) {
+            cout << "test" << endl;
+            break;
+        }
+
+        else if(var == 3) {
+            cout << "test" << endl;
+            break;
+        }
+        else {
+            cout << "Choice is invalid, please input onf of the choices below" << endl;
+            cout << endl << "Enter your choice of algorithm (Please type the number of your choice)" << endl
+            << "1. Uniform Cost Search" << endl
+            << "2. A* with the Misplaced Tile heuristic." << endl
+            << "3. A* with the Euclidean distance heuristic" << endl;
+            cin >> var;
+            
+        }
+    }
 /* 
+
     << "If you have a configuration please input the configuration values separated by commas from left to right and row by row"
     << endl << "Input the blank space as 0.\n\n" 
     << "For example an for an 8 puzzle that looks like this: \n\n2  4  6" << endl 
--- problem.h
+++ problem.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <map>
 #include <list>
 #include <string>
 #include "node.cpp"
@@ -11,23 +12,28 @@ using namespace std;
 
 class Problem {
     private:
-        Node* head;
-        vector<int> defaultState = {3,2,1,4,8,7,6,5,0};
-        void print();
-        Node* addMove(Node* head, vector<int> pos, vector<int> newData);
+        //Node* head;
+        vector<int> defaultState = {1,2,0,4,5,3,7,8,6};
+        void print(Node*);
+        Node* addMove(Node* head, vector<int> pos, vector<int> newData); // might not need; just change to expand node and do a findnode check
+        Node* findNode(Node *);
+
+        //operations
         vector<int> moveup(vector<int>);
         vector<int> movedown(vector<int>);
         vector<int> moveright(vector<int>);
         vector<int> moveleft(vector<int>);
 
     public:
+    Node *head;
+        map<vector<int>, bool> visited;
         Problem();
         Problem(vector<int> init);
         vector<int> initialState;
         vector<int> goalState = {1,2,3,4,5,6,7,8,0};
         queue <Node*> expandNode(Node* oldNode);
         bool checksolution(vector<int>&);
-        void printMoves();
+        void printPath(Node*);
 };
 
 #endif //PROBLEM_H_
